75367cc229c1b0c4c9241c19a7f3eddc53f2068b
Abdelkarim Nakkahi <nakkahi.abdelkarim@gmail.com>
2020-08-29 12:49:12 +0000

    Complete my portfolio website

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+let lastTopScroll = 0 ;
+const navbar = document.querySelector('nav');
+
+window.addEventListener('scroll',function(){
+    const topScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if(topScroll > lastTopScroll){
+        navbar.style.top = '-5rem'
+    }
+    else{
+        navbar.style.top = '0';
+        navbar.style.background = '#0B1741';
+        navbar.style.boxShadow = '0 5px 15px -5px #0B1741';
+    }
+    lastTopScroll = topScroll;
+});
+
+
+const menu = document.querySelector('.menu');
+const ul = document.querySelector('nav ul');
+
+menu.addEventListener('click', function(e){
+    ul.classList.toggle("active");
+});
+
+const link = document.querySelectorAll('nav ul a');
+console.log(link);
+for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', function(){
+        ul.classList.toggle("active");
+    });
+}
+
+// Animation on scroll
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".card");
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+  };
+  
+  const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
