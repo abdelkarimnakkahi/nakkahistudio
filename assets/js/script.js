0536edbb44fbe023169c038a884c55dbbb3fcfca
Abdelkarim Nakkahi <nakkahi.abdelkarim@gmail.com>
2021-03-29 15:12:25 +0000

    Fix some stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,28 @@
 let lastTopScroll = 0;
-const navbar = document.querySelector('nav');
+const navbar = document.querySelector("nav");
 
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
   const topScroll = window.pageYOffset || document.documentElement.scrollTop;
   if (topScroll > lastTopScroll) {
-    navbar.style.top = '-5rem'
+    navbar.style.top = "-5rem";
   } else {
-    navbar.style.top = '0';
-    navbar.style.background = '#070e27';
-    navbar.style.boxShadow = '0 5px 15px -5px #070e27';
+    navbar.style.top = "0";
+    navbar.style.background = "#080c20";
+    navbar.style.boxShadow = "0 5px 15px -5px #080c20";
   }
   lastTopScroll = topScroll;
 });
 
+const menu = document.querySelector(".menu");
+const ul = document.querySelector("nav ul");
 
-const menu = document.querySelector('.menu');
-const ul = document.querySelector('nav ul');
-
-menu.addEventListener('click', function (e) {
+menu.addEventListener("click", function (e) {
   ul.classList.toggle("active");
 });
 
-const link = document.querySelectorAll('nav ul a');
-console.log(link);
+const link = document.querySelectorAll("nav ul a");
 for (let i = 0; i < link.length; i++) {
-  link[i].addEventListener('click', function () {
+  link[i].addEventListener("click", function () {
     ul.classList.toggle("active");
   });
 }
@@ -36,53 +34,53 @@ const sliders = document.querySelectorAll(".card");
 
 const appearOptions = {
   threshold: 0,
-  rootMargin: "0px 0px -250px 0px"
+  rootMargin: "0px 0px -250px 0px",
 };
 
 const appearOnScroll = new IntersectionObserver(function (
-    entries,
-    appearOnScroll
-  ) {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) {
-        return;
-      } else {
-        entry.target.classList.add("appear");
-        appearOnScroll.unobserve(entry.target);
-      }
-    });
-  },
-  appearOptions);
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
 
-faders.forEach(fader => {
+faders.forEach((fader) => {
   appearOnScroll.observe(fader);
 });
 
-sliders.forEach(slider => {
+sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
 });
 
 // Scroll Animation
 
 const sr = ScrollReveal({
-  distance: '1rem',
+  distance: "1rem",
   duration: 1500,
-  reset: false
+  reset: false,
 });
 
-sr.reveal('.card', {
-  origin: 'top',
-  interval: 200
+sr.reveal(".card", {
+  origin: "top",
+  interval: 200,
 });
 
-sr.reveal('.message', {
-  origin: 'left',
-  interval: 200
+sr.reveal(".message", {
+  origin: "left",
+  interval: 200,
 });
 
-sr.reveal('.message-info', {
-  origin: 'right',
-  interval: 200
+sr.reveal(".message-info", {
+  origin: "right",
+  interval: 200,
 });
 
 // Get Current Year
@@ -90,4 +88,6 @@ sr.reveal('.message-info', {
 const fullYear = new Date();
 const currentYear = fullYear.getFullYear();
 
-document.querySelector('.current-year').textContent = currentYear;
+document.querySelector(".current-year").textContent = currentYear;
+
+
